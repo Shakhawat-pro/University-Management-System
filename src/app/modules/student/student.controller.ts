9b94eb1b90791ee28ab43abb2a98d7f1e950b87d
Shakhawat-pro <ratulhossain2692004@gmail.com>
2024-12-06 19:24:16 +0600

    Resoleve try-catch repetiton with higher Order function = catchAsync

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,58 +1,51 @@
-import { NextFunction, Request, Response } from 'express';
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { studentService } from './student.service';
 import sendResponse from '../../utils/sendResponse';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
+
+// Higher Order Function
+const catchAsync = (fn: RequestHandler) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(err => next(err))
+  }
+}
 
 
-const getAllStudent = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const result = await studentService.getAllStudentFromDB();
+const getAllStudent = catchAsync(async (req, res, next) => {
+  const result = await studentService.getAllStudentFromDB();
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: 'Student are Retrieved Successfully',
+    data: result,
+  })
+});
+
+
+const getSingleStudent: RequestHandler = catchAsync(async (req, res, next) => {
+  const studentId = req.params.id;
+  const result = await studentService.getSingleFromDB(studentId);
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: 'Student is Retrieved Successfully',
+    data: result,
+  })
+})
+
+
+const deleteStudent: RequestHandler = catchAsync(async (req, res, next) => {
+  const studentId = req.params.id;
+  const result = await studentService.deleteStudentFromDB(studentId);
+  if (result) {
     sendResponse(res, {
       statusCode: 200,
       success: true,
-      message: 'Student are Retrieved Successfully',
+      message: 'Student is Deleted Successfully',
       data: result,
     })
-  } catch (err) {
-    next(err)
-    // res.status(400).json({
-    //   success: false,
-    //   massage: err.message || "Something went wrong",
-    //   error: err,
-    // });
   }
-};
-
-const getSingleStudent = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const studentId = req.params.id;
-    const result = await studentService.getSingleFromDB(studentId);
-    sendResponse(res, {
-      statusCode: 200,
-      success: true,
-      message: 'Student is Retrieved Successfully',
-      data: result,
-    })
-  } catch (err) {
-    next(err)
-  }
-};
-
-const deleteStudent = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const studentId = req.params.id;
-    const result = await studentService.deleteStudentFromDB(studentId);
-    if (result) {
-      sendResponse(res, {
-        statusCode: 200,
-        success: true,
-        message: 'Student is Deleted Successfully',
-        data: result,
-      })
-    }
-  } catch (err) {
-    next(err)
-  }
-};
+})
 
 export const studentControllers = {
   getAllStudent,
